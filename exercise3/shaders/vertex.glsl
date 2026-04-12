d985e22b399b28d1c4f4949c8f0163367332d44e
Emanum <manuel.eiweck@emanum.dev>
2026-04-12 18:33:58 +0200

    Task3 Taper

--- exercise3/shaders/vertex.glsl
+++ exercise3/shaders/vertex.glsl
@@ -10,9 +10,28 @@ out vec3 worldPosition;
 
 vec4 taper(vec4 pos, float k) {
     //TASK1 - begin
+    
+    // Transform matrix:
+    //     s(z)  0    0
+    // T =  0   s(z)  0
+    //      0   0     1
+    //
 
-    return pos; // just here to avoid compile errors
+    //s(z) =   (max z - z)
+    //         -----------
+    //       (max z - min z)
 
+
+    // Base the scaling factor on the Y axis (height) instead of Z
+    float s_z = (maxCoord.y - pos.y) / (maxCoord.y - minCoord.y);
+    float s = mix(1.0, s_z, k);
+    mat4 T = mat4  (s, 0, 0, 0,
+                    0, 1, 0, 0, 
+                    0, 0, s, 0,
+                    0, 0, 0, 1);
+    
+    vec4 erg = T * pos;
+    return erg;
     //TASK1 - end
 }
 
